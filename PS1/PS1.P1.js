fb8c4365c00e510b2698c19a9855d74bfda15304
boredcod <84103200+boredcod@users.noreply.github.com>
2022-09-19 16:28:27 -0400

    part 2 done

--- PS1/PS1.P1.js
+++ PS1/PS1.P1.js
@@ -1,1 +1,6 @@
-//hello
+function biggie (s) {
+    s = s.split('').sort().reverse().join('')
+
+    return s
+}
+console.log(`Reverse alphabet is: ${biggie('supercalifragilisticexpialidocious')}`)
---
+++ PS1/PS1.P2.js
@@ -0,0 +1,51 @@
+function evaluate (s){
+    let pointer = 0;
+    let firstNum = "";
+    let operator = ""
+    let secondNum = "";
+    while (pointer < s.length){
+        if (!isNaN(s[pointer])){
+            firstNum += s[pointer]
+        }
+        else {
+            break
+        }
+        pointer++
+    }
+    operator = s[pointer]
+    pointer++
+    while (pointer < s.length){
+        secondNum += s[pointer];
+        pointer++;
+    }
+    firstNum = parseInt(firstNum);
+    secondNum = parseInt(secondNum);
+    let res = 0;
+    switch (operator) {
+        case '+':
+            res = firstNum + secondNum;
+            break;
+        case '-':
+            res = firstNum - secondNum;
+            break;
+        case '*':
+            res = firstNum * secondNum;
+            break;
+        case '^':
+            res = firstNum ** secondNum;
+            break;
+        case '*':
+            res = firstNum / secondNum;
+            break;
+        case '%':
+            res = firstNum % secondNum;
+            break;
+        default:
+            break;
+
+    }
+    return res
+}
+const expression = '8%3';
+let operator = evaluate(expression);
+console.log(`${expression} = ${operator}`)
